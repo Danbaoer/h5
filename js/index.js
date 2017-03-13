f5b488d300cdc2bc942a2366c108e76557c451b4
Danbaoer <2908492755@qq.com>
2017-03-13 11:23:43 +0800

    finished

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 /**
  * Created by asus on 2017/3/13.
  */
-
-$("#ols").find("li:last-child").on("click",function(){
+$("#ols li").last().on("click",function(){
     $(".add").show();
+})
+$("#btn").on("click",function(){
+    var val=$("#text").val();
+    if(val==""){
+        alert("请输入添加的内容")
+    }else if(val.length>5){
+        alert("字数不能多于5个")
+        $("input").val("");
+    }else{
+        $("#ols li").last().before('<li class="col-md-3 text-center">'+val+'<i class="fa fa-times-circle"></i></li>');
+        $("input").val("");
+        $(".add").hide();
+    }
 })
